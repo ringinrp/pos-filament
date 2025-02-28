83923d47fbdffe06b73ae45c76a80c64d5aa4ffe
ringinrp <ringinrp21@gmail.com>
2025-02-28 13:15:17 +0700

    update relasi

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    //1 category dapat memiliki banyak product
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PaymentMethod extends Model
 {
-    //
+    use HasFactory;
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
