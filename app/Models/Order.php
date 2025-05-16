1ac31a10fabad0750b724024bf956ed0487eba79
ringinrp <ringinrp21@gmail.com>
2025-05-16 08:17:10 +0700

    update: model, migration, resource

---
+++ app/Models/Order.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public function paymentMethod(): BelongsTo
+    {
+        return $this->belongsTo(PaymentMethod::class);
+    }
+
+    //order bisa banyak order product
+    public function OrderProducts(): HasMany
+    {
+        return $this->hasMany(OrderProduct::class);
+    }
+}
---
+++ app/Models/PaymentMethod.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class PaymentMethod extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'image',
+        'is_cash'
+    ];
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+}
